be132198faece4ba1aae19e355feb39e06f96d4d
Vëx <veljko.tekelerovic@gmail.com>
2024-11-04 23:15:01 +0100

    Content rework and improvements
    Added "check source code" button on main page.
    Minor CSS adjustments.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,26 @@
-<script>
-    import { goto } from "$app/navigation";
-
-    async function discordNavigation() {
-        window.location.href = "https://discord.gg/dzbsT8jTg5";
-    }
-</script>
-
-<section>
+<main>
     <img src="/favicon.png" height="100" alt="vincoin_logo"/>
     <h1>VINCOIN</h1>
     <i>Modern currency for advanced mindset</i>
 
-    <div>
-        <button on:click|self={() => goto("/learn-more")}>Learn More</button>
-        <button on:click|self={() => discordNavigation()} >
+    <section>
+        <a href="/learn-more">
+            <img src="/learn-more.svg" alt="learn_more" height="20"/>
+            <span>Learn More</span>
+        </a>
+        <a href="https://github.com/vexy/vincoin">
+            <img src="/github-mark.svg" alt="github_logo" height="20"/>
+            <span>Source code</span>
+        </a>
+        <a href="https://discord.gg/dzbsT8jTg5">
             <img src="/discord-mark-blue.svg" alt="discord_logo" height="20"/>
             <span>Join our Discord</span>
-        </button>
-    </div>
-</section>
+        </a>
+    </section>
+</main>
 
 <style>
-    section {
+    main {
         height: 100vh;
         margin: 0;
 
@@ -37,37 +36,38 @@
         margin: 10px;
     }
 
+    i {
+        margin-bottom: 15px;
+    }
+
     img {
         border-radius: 15px;
     }
 
-    i {
-        margin: 10px;
-    }
-
-    div {
-        display: flex;
-        justify-content: center;
-        gap: 15px;
-    }
-
-    button {
+    a {
         border: none;
         border-radius: 10px;
-        padding: 0.65rem;
+        padding: 0.55rem;
         margin: 10px;
+        background: #cfc4c0;
+        color: rgb(81, 81, 81);
 
         display: flex;
+        justify-content: center;
         align-items: center;
         gap: 5px;
 
-        font-family: "Overpass", sans-serif;
-        font-optical-sizing: auto;
+        text-decoration: none;
     }
 
-    button:hover {
+    a:hover {
         font-style: italic;
         font-weight: 300;
-        color: rgb(0, 119, 79);
+        color: rgb(26, 195, 139);
+        background: rgb(54, 187, 142);
+    }
+
+    a:visited {
+        color: rgb(81, 81, 81);
     }
 </style>
--- src/routes/learn-more/+page.svelte
+++ src/routes/learn-more/+page.svelte
@@ -1,6 +1,16 @@
 <svelte:head>
-    <title>VINCOIN - Learn more</title>
+    <title>VINCOIN - Details</title>
     <meta name="keywords" content="learn, more, vincoin, mining, cryptography, economy, denominations">
+
+    <!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-KG2S7BGDGH"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'G-KG2S7BGDGH');
+	</script>
 </svelte:head>
 
 <img src="/favicon.png" height="75" alt="vincoin_logo" />
@@ -12,7 +22,7 @@ It is designed to be advanced variation of the Bitcoin but with several marginal
 
 <h2>Mining philosophy</h2>
 <p>Using block reward as "incentive" for people to start spreading Bitcoin network proved to be degenerative perception of the free and open sourced software.</p>
-<p>Earning, selling and buying pieces of Bitcoin is actually <b>nowhere</b> to be found in <a href="https://bitcoin.org/bitcoin.pdf">original whitepaper</a>, technical documentation nor single line of code, so it can hardly be explained why most of the people assume Bitcoin actually have any value. Specially one expressed in other fiat currencies...</p>
+<p>Earning, selling and buying pieces of Bitcoin is actually <b>nowhere</b> to be found in <a href="https://bitcoin.org/bitcoin.pdf">original whitepaper</a>, technical documentation nor single line of code, so it can hardly be explained why most of the people assume Bitcoin actually have any market price. Specially one expressed in other fiat currencies... 😐</p>
 <p>With that in mind, VINCOIN <u>does not award</u> node runners with any block reward(s), eliminating the possibility for similar destiny of Bitcoin "market offerings" and shallow perception.</p>
 
 <h2>Advanced cryptography</h2>
@@ -21,36 +31,40 @@ It is designed to be advanced variation of the Bitcoin but with several marginal
 <p>With that huge difference in architecture, naturally many technical aspect of Vincoin are different from the beginning.</p>
 <p>Above all, VINCOIN does apply some of the ZKP scheme, allowing it's users even more possibilities and use cases.</p>
 
-<h2>Starting and total amount of VINCOINS</h2>
+<h2>Economical setup</h2>
+
+<h3>Starting and total amount of VINCOINS</h3>
 <p>As it is technically possible, VINCOIN is programmed to "award" any new wallet with starting sum of VINCOINS, untill all the VINCOINS are depleted.</p>
 <p>That is made possible using different signing and cryptography algorithms and OPCODE commands.</p>
 <p>Similarly to Bitcoin, VINCOIN as well has fixed amount of units of exchange, currently floating around 11-17 milions of coins.</p>
 <p>Currently, Vincoin algorithms will award 1 <code>VIN</code> to every new wallet/user.</p>
 <p>Advanced ZkP scheme will assure only one coin per wallet can be given.</p>
 
-<h2>Economical Denominations</h2>
+<h3>Economical Denominations</h3>
 <p>To date, VINCOIN uses following economical denominations:</p>
 <ul>
     <li>Vinčanac (<code><b>VIN</b></code>)</li>
     <li>Lepenac (<code><b>LEP</b></code>)</li>
 </ul>
 <p>
-    Current setup goes aims to be: 
+    Current setup aims to be:<br>
     <code>1 VIN = 1_000_000 LEP</code>
 </p>
 
 <h2>Open sourced</h2>
 <p>VINCOIN is completely open sourced. It is released under MIT license.</p>
-<p>Check VINCOIN codebase at: <a href="https://github.com/vexy/vincoin">Vincoin @ Github</a></p>
-<p>VINCOIN is in (very!) early stages of it's development, so expect a lot of changes and adjustments</p>
-
-<h2>Contribution</h2>
-<p>Contributions are more than welcome. Feel free to join our Github for code contributions.</p>
+<p>Feel free to checkout our Github: <a href="https://github.com/vexy/vincoin">Vincoin @ Github</a>. Contributions are more than welcome 🙌</p>
 <p>Also, check out our <a href="https://discord.gg/dzbsT8jTg5">Discord server</a> for more info and community engagement.</p>
+
+<p><i>VINCOIN is in (very!) early stages of it's development, so expect a lot of changes and adjustments</i></p>
 
 <style>
     h1, h2 {
         text-align: center;
+    }
+
+    h3 {
+        margin-left: 10px;
     }
 
     h1:hover, h2:hover {
