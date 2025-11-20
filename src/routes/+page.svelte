b45f9240651be3798e9ea1690ec3bfd9e7311d16
Vëx <veljko.tekelerovic@gmail.com>
2025-11-20 16:39:18 +0100

    Added new pages, assets, styling and content
    Squashed commits:
    
    commit a2d200be3b00bf927ca80773853f1a2c5b3af78c
    commit a7285a76d00d814effb638dab2f36f4f4abc0968
    commit 6a10f476de4360dd3c70eb378e03e1d79c32abcd
    commit 3f00e48581ea4be0d8b5d5720fb2faa97041b6ce
    commit ec6c9ef86435c2746bffbe70a3b60796a691a46c

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,18 @@
 <main>
-    <img src="/favicon.png" height="100" alt="vincoin_logo"/>
     <h1>VINCOIN</h1>
     <i>Modern currency for advanced mindset</i>
 
     <section>
         <a href="/learn-more">
-            <img src="/learn-more.svg" alt="learn_more" height="20"/>
+            <img src="images/learn-more.svg" alt="learn_more" height="20"/>
             <span>Learn More</span>
         </a>
         <a href="https://github.com/vexy/vincoin">
-            <img src="/github-mark.svg" alt="github_logo" height="20"/>
+            <img src="logos/github-mark.svg" alt="github_logo" height="20"/>
             <span>Source code</span>
         </a>
         <a href="https://discord.gg/dzbsT8jTg5">
-            <img src="/discord-mark-blue.svg" alt="discord_logo" height="20"/>
+            <img src="logos/discord-mark-blue.svg" alt="discord_logo" height="20"/>
             <span>Join our Discord</span>
         </a>
     </section>
@@ -21,27 +20,23 @@
 
 <style>
     main {
-        height: 100vh;
         margin: 0;
+        height: 100vh;
 
         display: flex;
         flex-direction: column;
         justify-content: center;
         align-items: center;
-    }
 
-    h1 {
-        font-size: 3rem;
-        text-align: center;
-        margin: 10px;
+        color: #cecece;
+
+        background-image: url("/images/vincoin15.png");
+        background-repeat: no-repeat;
+        background-position: center;
     }
 
     i {
         margin-bottom: 15px;
-    }
-
-    img {
-        border-radius: 15px;
     }
 
     a {
@@ -49,7 +44,7 @@
         border-radius: 10px;
         padding: 0.55rem;
         margin: 10px;
-        background: #cfc4c0;
+        background: #afaba9;
         color: rgb(81, 81, 81);
 
         display: flex;
@@ -63,11 +58,8 @@
     a:hover {
         font-style: italic;
         font-weight: 300;
+        scale: 0.95;
         color: rgb(26, 195, 139);
         background: rgb(54, 187, 142);
-    }
-
-    a:visited {
-        color: rgb(81, 81, 81);
     }
 </style>
---
+++ src/routes/technical/+page.svelte
@@ -0,0 +1,47 @@
+<h1>Technical aspects of Vincoin</h1>
+
+<div>
+    <img src="/images/vincoin4.png" width="100" alt="coins" />
+    <img src="/images/vincoin5.png" width="100" alt="coins" />
+</div>
+
+<p>Learn more about architectural basics before diving into more complex aspects of Vincoin</p>
+
+<h2>Post quantum cryptography</h2>
+<p>Compared to all the cryptography contained in Bitcoin, Vincoin took radically different approach.</p>
+<p>Yes, it's born in 2018 so it had plenty of time to observe <a href="https://www.nist.gov/">NIST</a> efforts in formulating quantum resistant algorithms, which explains VINCOIN tendention to use only quantum resistant algorithms and nothing more.</p>
+<p>With that huge difference in architecture, naturally many technical aspect of Vincoin are different from the beginning.</p>
+
+<div class='description'>
+    <h4>🔜 Content comming soon 🔜</h4>
+    <h2>Data processing (aka mining)</h2>
+    <h2>Data storage (aka blockchain)</h2>
+    <h2>System-user interaction (aka wallet)</h2>
+    <h2>Data integrity</h2>
+    <p>Above all, VINCOIN does apply some of the ZKP scheme, allowing it's users even more possibilities and use cases.</p>
+</div>
+
+<h2>Open sourced</h2>
+<p><code>Vincoin</code> is completely open sourced, released under MIT license.</p>
+<p>Feel free to browse our <a href="https://github.com/vexy/vincoin">Github repo</a> (<i>stick to the development branch</i>). Contributions are more than welcome 🙌</p>
+<p><b>Please note:</b><i>VINCOIN is in (very!) early stages of it's development, so expect <b>a lot</b> of changes and adjustments</i></p>
+
+<style>
+    div {
+        display: flex;
+        justify-content: center;
+        gap: 1rem;
+    }
+
+    .description {
+        display: flex;
+        flex-direction: column;
+        gap: 0rem;
+
+        border: 0.5px dotted gray;
+    }
+
+    h4 {
+        text-align: center;
+    }
+</style>
